9213c8889ace4ae6e72644ece7ad61c9b6208d5d
Pedro922 <82282069+eipedrov@users.noreply.github.com>
2021-04-21 21:23:11 -0400

    Aula 03

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/global.scss'
+
+import { Header } from "../components/Header/Index"
+import { Player } from "../components/Player/Index"
+
+
+import styles from '../styles/app.module.scss'
+import React from "react";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp
